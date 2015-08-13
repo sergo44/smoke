77883c0bbb8bee2d5ae1b4081edf4ce920cb1878
sergo44 <sergo44@mail.ru>
2015-08-13 09:34:24 +0300

    Add smoke script to build CURL

---
+++ curl.sh
@@ -0,0 +1,22 @@
+SRC="/usr/src/curl/7_44_0"
+
+if [ ! -d $SRC ];
+then
+    mkdir -p $SRC
+    git clone https://github.com/bagder/curl $SRC
+
+    cd $SRC
+    git checkout tags/curl-7_44_0
+else
+    cd $SRC
+    git pull
+fi;
+
+./buildconf
+./configure --prefix=/opt/curl --with-ssl=/opt/openssl --disable-file --without-pic --disable-shared --without-ca-bundle --with-ca-path=/opt/openssl/ssl/certs
+
+read -p "Press enter to make or ^C to stop?" REPLY
+make -j10
+
+read -p "Press enter to make install or ^C to break?" REPLY
+make install
