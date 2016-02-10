83d900c5b8e03e3af4fbd45507a440d79ab073ec
sergo44 <sergo44@mail.ru>
2016-02-10 15:15:51 +0300

    Fix URL

--- php-test-tlsv1_2.php
+++ php-test-tlsv1_2.php
@@ -1,6 +1,7 @@
 <?php
 
 $ch = curl_init("https://engine.paymentgate.ru/payment/webservices/merchant-ws?wsdl");
+#$ch = curl_init("https://test.paymentgate.ru:8443/payment/webservices/merchant-ws?wsdl");
 curl_setopt($ch, CURLOPT_VERBOSE, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURL_SSLVERSION_TLSv1_2, 1);
